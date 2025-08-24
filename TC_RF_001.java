405c8d538e14992349ac7b5626758051b66dbafb
Phani4503 <d.phaniendra@gmail.com>
2025-08-24 10:46:46 +0530

    first commit

---
+++ TC_RF_001.java
@@ -0,0 +1,50 @@
+package register;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import genericUtility.DateAddingwithemail;
+
+public class TC_RF_001 {
+	@Test
+	public void verifyRegisteringAccountUsingMandateryfields() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.id("input-firstname")).sendKeys("phani");
+		driver.findElement(By.id("input-lastname")).sendKeys("kumar");
+		driver.findElement(By.id("input-email")).sendKeys(DateAddingwithemail.generateBrandNewEmail());
+		driver.findElement(By.name("telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//a[@class='list-group-item'][text()='Logout']")).isDisplayed());
+		
+		
+	}
+	public static String generateBrandNewEmail()
+	{
+		Date date = new Date();
+		String dateString = date.toString();
+		String dateStringWithoutSpaces = dateString.replaceAll("\\s","");
+		String dateStringWithoutSpacesAndColons = dateStringWithoutSpaces.replaceAll("\\:","");
+		String brandNewEmail = dateStringWithoutSpacesAndColons+"@gmail.com";
+		return brandNewEmail;
+		
+	}
+}
